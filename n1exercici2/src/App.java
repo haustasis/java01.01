0b53f701151160047b4df0f416bff9f8a8a2b114
TARISO <toni.ariso@gmail.com>
2022-02-12 06:11:52 +0100

    Clean ex2

--- n1exercici2/src/App.java
+++ n1exercici2/src/App.java
@@ -1,8 +1,3 @@
-/*
-Exercici 2. A la classe Vehicle afegeix dos mètodes: un static anomenat parar() i un altre no-static anomenat accelerar().
- Demostra com invocar el mètode static i el no-static des del main() (per exemple, enviant un missatge a la consola).
-
- */
 
 public class App {
 
--- n1exercici2/src/Vehicle.java
+++ n1exercici2/src/Vehicle.java
@@ -1,17 +1,9 @@
-/*
-Exercici 2. A la classe Vehicle afegeix dos mètodes: un static anomenat parar() i un altre no-static anomenat accelerar().
- Demostra com invocar el mètode static i el no-static des del main() (per exemple, enviant un missatge a la consola).
 
- */
 
 public class Vehicle {
 
     public Vehicle() {
-       // System.out.println("[-] Metode constructor Vehicle class");
-    }
 
-    public void iniciar() {
-        System.out.println("[+] Metode Iniciar Vehicle class");
     }
 
     public static void parar() {
