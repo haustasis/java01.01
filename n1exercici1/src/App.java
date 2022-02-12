63afcd26379eae301f584e4ffba23752c3c35f94
TARISO <toni.ariso@gmail.com>
2022-02-12 04:41:22 +0100

    Add main and create Vehicle

--- n1exercici1/src/App.java
+++ n1exercici1/src/App.java
@@ -1,6 +1,12 @@
 /*
-Exercici 1. Crea una classe Vehicle amb un constructor i un mètode anomenat iniciar(), que es invocat pel main() (aquest mètode pot estar en la mateixa classe o en una classe apart) . Demostra la seqüència d'execució dels mètodes de la classe (per exemple, enviant un missatge a la consola).
+Exercici 1. Crea una classe Vehicle amb un constructor i un mètode anomenat iniciar(),
+que es invocat pel main() (aquest mètode pot estar en la mateixa classe o en una classe apart) .
+Demostra la seqüència d'execució dels mètodes de la classe (per exemple, enviant un missatge a la consola).
  */
 
 public class App {
+
+    public static void main(String[] args) {
+
+    }
 }
